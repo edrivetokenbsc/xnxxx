e6f8a17f95d74f022c8f7f41f8f1376cecad0ef0
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-08 14:14:07 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,7 @@ from flask_socketio import SocketIO, emit
 import subprocess
 import threading
 import eventlet
+import re
 
 eventlet.monkey_patch()
 
@@ -12,18 +13,16 @@ socketio = SocketIO(app, async_mode='eventlet')
 
 def execute_command():
     process = subprocess.Popen(['bash', '-c', 'ls && cd build && python main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    def read_output(pipe, type):
-        while True:
-            line = pipe.readline()
-            if not line:
-                break
-            socketio.emit('log', {'message': line.strip(), 'type': type}, broadcast=True)
-    stdout_thread = threading.Thread(target=read_output, args=(process.stdout, 'success'))
-    stderr_thread = threading.Thread(target=read_output, args=(process.stderr, 'error'))
-    stdout_thread.start()
-    stderr_thread.start()
-    stdout_thread.join()
-    stderr_thread.join()
+    while True:
+        output = process.stdout.readline() + process.stderr.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            log_type = 'error' if re.search(r'\bERROR\b|\bTraceback\b', output) else 'success'
+            socketio.emit('log', {'message': output.strip(), 'type': log_type}, broadcast=True)
+
+    process.stdout.close()
+    process.stderr.close()
     process.wait()
 
 @app.route('/')
