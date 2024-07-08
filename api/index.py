3b1a5b9805daa421f2c4e23b05203e1565f77328
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-08 13:34:19 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
-from flask_sse import sse
+from flask import Flask, render_template, jsonify
 import subprocess
 import threading
-import eventlet
+import time
 
 app = Flask(__name__)
-app.config["REDIS_URL"] = "redis://default:AZY1AAIncDFlZjkxZTczYzRkNzA0Mzg4OWQyOGE0YmUwMzAwMjhlNnAxMzg0NTM@climbing-lemming-38453.upstash.io:6379"
-app.register_blueprint(sse, url_prefix='/stream')
+
+logs = []
+
+def execute_command():
+    global logs
+    process = subprocess.Popen(['python', 'build/main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    logs = []
+    for line in process.stdout:
+        logs.append(line.strip())
+    process.stdout.close()
+    process.wait()
 
 @app.route('/')
 def home():
@@ -14,16 +22,13 @@ def home():
 
 @app.route('/run-command')
 def run_command():
-    def execute_command():
-        process = subprocess.Popen(['python build/main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        for line in process.stdout:
-            sse.publish({"message": line.strip()}, type='greeting')
-        process.stdout.close()
-        process.wait()
-
     thread = threading.Thread(target=execute_command)
     thread.start()
     return "Command is running..."
+
+@app.route('/logs')
+def get_logs():
+    return jsonify(logs)
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
