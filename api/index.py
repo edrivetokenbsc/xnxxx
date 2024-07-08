3f6ea736a64a018b4619f494568a7f271514c51e
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-08 13:21:13 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sse import sse
+import subprocess
+import threading
+import eventlet
 
 app = Flask(__name__)
+app.config["REDIS_URL"] = "redis://default:AZY1AAIncDFlZjkxZTczYzRkNzA0Mzg4OWQyOGE0YmUwMzAwMjhlNnAxMzg0NTM@climbing-lemming-38453.upstash.io:6379"
+app.register_blueprint(sse, url_prefix='/stream')
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/run-command')
+def run_command():
+    def execute_command():
+        process = subprocess.Popen(['nproc'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        for line in process.stdout:
+            sse.publish({"message": line.strip()}, type='greeting')
+        process.stdout.close()
+        process.wait()
+
+    thread = threading.Thread(target=execute_command)
+    thread.start()
+    return "Command is running..."
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
