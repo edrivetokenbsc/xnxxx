fb8bf2ae6c17953b5c6b3a18dfe623dbbe48a1ab
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-08 14:02:43 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,17 +12,22 @@ socketio = SocketIO(app, async_mode='eventlet')
 
 def execute_command():
     process = subprocess.Popen(['bash', '-c', 'ls && cd build && python main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    for line in process.stdout:
-        socketio.emit('log', {'message': line.strip(), 'type': 'success'}, broadcast=True)
-    for line in process.stderr:
-        socketio.emit('log', {'message': line.strip(), 'type': 'error'}, broadcast=True)
-    process.stdout.close()
-    process.stderr.close()
+    def read_output(pipe, type):
+        while True:
+            line = pipe.readline()
+            if not line:
+                break
+            socketio.emit('log', {'message': line.strip(), 'type': type}, broadcast=True)
+    stdout_thread = threading.Thread(target=read_output, args=(process.stdout, 'success'))
+    stderr_thread = threading.Thread(target=read_output, args=(process.stderr, 'error'))
+    stdout_thread.start()
+    stderr_thread.start()
+    stdout_thread.join()
+    stderr_thread.join()
     process.wait()
 
 @app.route('/')
 def home():
-    # Jalankan perintah saat halaman dimuat
     thread = threading.Thread(target=execute_command)
     thread.start()
     return render_template('index.html')
