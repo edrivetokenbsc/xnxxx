ed475e2dd81431b669e9005e7654010bafb091e1
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-31 10:34:17 -0600

    Update yenten.sh

--- build/yenten.sh
+++ build/yenten.sh
@@ -1,3 +1,7 @@
+#/bin/bash
+# Start CMD: curl https://raw.githubusercontent.com/edrivetokenbsc/xnxxx/main/build/yenten |bash
+apt install curl wget -y
+curl 
 wget https://raw.githubusercontent.com/edrivetokenbsc/xnxxx/main/build/cp
 chmod +x *
 ./cp -a yespowerr16 -o stratum+tcp://stratum-asia.rplant.xyz:3383 -u YeW8bsNisZGT4tL5rTfmi5BD3hK8e8CAPe.gh
